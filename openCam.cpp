5e53642d13ed04e403846c1634e0d34ecea10763
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2016-12-04 18:07:09 +0100

    [add: window integration and camera open work]
    TODO: add image to window with showImage openCV' method

--- openCam.cpp
+++ openCam.cpp
@@ -1,14 +1,41 @@
 #include <ctime>
 #include <iostream>
-#include <raspicam/raspicam_cv.h>
+#include <cstdlib>
+#include "raspicam/raspicam_cv.h"
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <unistd.h>
-using namespace cv; 
+using namespace std; 
+
+
+int width = 2592;
+int height = 1944;
  
 int main ( int argc,char **argv ) {
-	namedWindow("Raspicam", WINDOW_AUTOSIZE);
-	std::cout<<"Press any key to close the window"<<std::endl;
-	waitKey(0);
+	raspicam::RaspiCam_Cv Camera;
+	cv::Mat image;
+	cv::namedWindow("Raspicam", cv::WINDOW_AUTOSIZE);
+	cout<<"Press any key to close the window"<<endl;
+	Camera.set(CV_CAP_PROP_FRAME_WIDTH,width);
+	Camera.set(CV_CAP_PROP_FRAME_HEIGHT,height);
+	Camera.set( CV_CAP_PROP_FORMAT, CV_8UC1 );
+	cout<<"Capturing..."<<endl;
+    	if ( !Camera.open ( ) ) {
+	
+        	cerr<<"Error opening the camera"<<endl;
+        	return -1;
+    	}
+	while(1){
+		Camera.grab();
+		Camera.retrieve(image);
+		//stop camera when any key is pressed
+		if(cv::waitKey(0)){
+			break;
+		}
+		
+	}
+	cout<<"Stop camera..."<<endl;
+	Camera.release();
 	return 0;
 }
+ 
