50b8384a1ab6ec9f4eeadb41c89d84d3b95df018
kevinhassanig <kevin.hassan@etu.umontpellier.fr>
2016-12-19 21:24:25 +0100

    work: open camera in window with cpp code

--- openCam.cpp
+++ openCam.cpp
@@ -1,7 +1,9 @@
 #include <math.h>
 #include <iostream>
+#include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/core/utility.hpp"
+#include "opencv2/videoio/videoio.hpp"
 
 
 using namespace cv;
@@ -10,11 +12,18 @@ using namespace std;
 
 int main( int argc, char** argv)
 {
-  const char* window_title = "webcam-test";
+  const char* window_title = "capture-camera";
+  VideoCapture cap(0); // open the default camera
+  if(!cap.isOpened())  // check if we succeeded
+    return -1;
+
+  Mat edges;
   namedWindow(window_title,CV_WINDOW_AUTOSIZE);
   while(1){
-    char c = cvWaitKey(33);
-    if(c==27) break;
+    Mat frame;
+    cap >> frame;
+    imshow(window_title, frame);
+    if(waitKey(30) >= 0) break; //press any key to exit
   }
   destroyWindow(window_title) ;
   return 0;
