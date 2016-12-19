5acb73471bb874f3cb6c28ad63bc4bd9273a3155
kevinhassanig <kevin.hassan@etu.umontpellier.fr>
2016-12-19 21:24:02 +0100

    edit: compil script to add all opencv libraries

--- compilCam.sh
+++ compilCam.sh
@@ -1,3 +1,3 @@
 echo "Compilation du programme en cours ..."
-g++ $1 -o $2 -I/usr/local/include/ -lopencv_core -lopencv_highgui
+g++ $1 -o $2 -I/usr/local/include/ `pkg-config --libs opencv`
 echo "Compilation du programme termin√e avec succ√s"
