b39b640dc97374d8f134356914cbf3ab3ed6eae8
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2016-12-23 14:42:15 +0100

    fix: compilation script

--- compilCam.sh
+++ compilCam.sh
@@ -1,3 +1,3 @@
 echo "Compilation du programme en cours ..."
-g++ $1 -o $2 -I/usr/local/include/ `pkg-config --libs opencv`
+g++ $1 -o $2 ` pkg-config --cflags --libs opencv`
 echo "Compilation du programme termin√e avec succ√s"
