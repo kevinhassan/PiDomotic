fa363ab560a7aeb18ac086417c6bc139cc451439
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2017-01-21 21:51:36 +0100

    test#4

--- pi-home-surveillance/pi_surveillance.py
+++ pi-home-surveillance/pi_surveillance.py
@@ -22,50 +22,20 @@ args = vars(ap.parse_args())
 warnings.filterwarnings("ignore")
 conf = json.load(open(args["conf"]))
 
-# initialize the camera and grab a reference to the raw camera camerature
+# initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-#camera.resolution = tuple(conf["resolution"])
-#camera.framerate = conf["fps"]
-#rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
-# Change the camera setting using the set() function
-# camera.set(cv2.cv.CV_CAP_PROP_EXPOSURE, -6.0)
-# camera.set(cv2.cv.CV_CAP_PROP_GAIN, 4.0)
-# camera.set(cv2.cv.CV_CAP_PROP_BRIGHTNESS, 144.0)
-# camera.set(cv2.cv.CV_CAP_PROP_CONTRAST, 27.0)
-# camera.set(cv2.cv.CV_CAP_PROP_HUE, 13.0) # 13.0
-# camera.set(cv2.cv.CV_CAP_PROP_SATURATION, 28.0)
-# Read the current setting from the camera
-test = camera.get(cv2.cv.CV_CAP_PROP_POS_MSEC)
-ratio = camera.get(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO)
-frame_rate = camera.get(cv2.cv.CV_CAP_PROP_FPS)
-width = camera.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
-height = camera.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
-brightness = camera.get(cv2.cv.CV_CAP_PROP_BRIGHTNESS)
-contrast = camera.get(cv2.cv.CV_CAP_PROP_CONTRAST)
-saturation = camera.get(cv2.cv.CV_CAP_PROP_SATURATION)
-hue = camera.get(cv2.cv.CV_CAP_PROP_HUE)
-gain = camera.get(cv2.cv.CV_CAP_PROP_GAIN)
-exposure = camera.get(cv2.cv.CV_CAP_PROP_EXPOSURE)
-print("Test: ", test)
-print("Ratio: ", ratio)
-print("Frame Rate: ", frame_rate)
-print("Height: ", height)
-print("Width: ", width)
-print("Brightness: ", brightness)
-print("Contrast: ", contrast)
-print("Saturation: ", saturation)
-print("Hue: ", hue)
-print("Gain: ", gain)
-print("Exposure: ", exposure)
+camera.resolution = tuple(conf["resolution"])
+camera.framerate = conf["fps"]
+rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
 
 # allow the camera to warmup, then initialize the average frame, last
 # uploaded timestamp, and frame motion counter
-print "Lancement de la camera ..."
+print "Lancement de la caméra ..."
 avg = None
 motionCounter = False
 
-# camerature frames from the camera
-for f in camera.camerature_continuous(rawCapture, format="bgr", use_video_port=True):
+# capture frames from the camera
+for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image and initialize
 	frame = f.array
 
