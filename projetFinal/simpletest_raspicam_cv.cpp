ce7ae856a2b9c21ecf392f799671214d1b5c21a3
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2017-01-21 15:45:34 +0100

    try opening cam with raspicam lib

--- projetFinal/simpletest_raspicam_cv.cpp
+++ projetFinal/simpletest_raspicam_cv.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main ( int argc,char **argv ) {
     raspicam::RaspiCam Camera; //Cmaera object
-    //Open camera 
+    //Open camera
     cout<<"Opening Camera..."<<endl;
     if ( !Camera.open()) {cerr<<"Error opening camera"<<endl;return -1;}
     //wait a while until camera stabilizes
@@ -26,8 +26,7 @@ int main ( int argc,char **argv ) {
     outFile<<"P6\n"<<Camera.getWidth() <<" "<<Camera.getHeight() <<" 255\n";
     outFile.write ( ( char* ) data, Camera.getImageTypeSize ( raspicam::RASPICAM_FORMAT_RGB ) );
     cout<<"Image saved at raspicam_image.ppm"<<endl;
-    //free resrources    
+    //free resrources
     delete data;
     return 0;
 }
-
