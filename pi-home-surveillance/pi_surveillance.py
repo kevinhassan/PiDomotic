af63a229f5a981f32e158e176969b78499477be5
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2017-01-21 22:03:46 +0100

    test#4

--- pi-home-surveillance/pi_surveillance.py
+++ pi-home-surveillance/pi_surveillance.py
@@ -31,8 +31,7 @@ rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
 # allow the camera to warmup, then initialize the average frame, last
 # uploaded timestamp, and frame motion counter
 print "Lancement de la camera ..."
-camera.start_preview()
-avg = camera.capture()
+avg = camera.start_preview()
 motionCounter = False
 
 # capture frames from the camera
