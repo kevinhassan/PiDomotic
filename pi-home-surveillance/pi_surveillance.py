97bfb0a8eef7de971ddb18ebf0d1bc3eef03ee42
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2017-01-21 21:47:12 +0100

    test#4

--- pi-home-surveillance/pi_surveillance.py
+++ pi-home-surveillance/pi_surveillance.py
@@ -24,15 +24,45 @@ conf = json.load(open(args["conf"]))
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
-camera.resolution = tuple(conf["resolution"])
-camera.framerate = conf["fps"]
-rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
+#camera.resolution = tuple(conf["resolution"])
+#camera.framerate = conf["fps"]
+#rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
+# Change the camera setting using the set() function
+# cap.set(cv2.cv.CV_CAP_PROP_EXPOSURE, -6.0)
+# cap.set(cv2.cv.CV_CAP_PROP_GAIN, 4.0)
+# cap.set(cv2.cv.CV_CAP_PROP_BRIGHTNESS, 144.0)
+# cap.set(cv2.cv.CV_CAP_PROP_CONTRAST, 27.0)
+# cap.set(cv2.cv.CV_CAP_PROP_HUE, 13.0) # 13.0
+# cap.set(cv2.cv.CV_CAP_PROP_SATURATION, 28.0)
+# Read the current setting from the camera
+test = cap.get(cv2.cv.CV_CAP_PROP_POS_MSEC)
+ratio = cap.get(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO)
+frame_rate = cap.get(cv2.cv.CV_CAP_PROP_FPS)
+width = cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
+brightness = cap.get(cv2.cv.CV_CAP_PROP_BRIGHTNESS)
+contrast = cap.get(cv2.cv.CV_CAP_PROP_CONTRAST)
+saturation = cap.get(cv2.cv.CV_CAP_PROP_SATURATION)
+hue = cap.get(cv2.cv.CV_CAP_PROP_HUE)
+gain = cap.get(cv2.cv.CV_CAP_PROP_GAIN)
+exposure = cap.get(cv2.cv.CV_CAP_PROP_EXPOSURE)
+print("Test: ", test)
+print("Ratio: ", ratio)
+print("Frame Rate: ", frame_rate)
+print("Height: ", height)
+print("Width: ", width)
+print("Brightness: ", brightness)
+print("Contrast: ", contrast)
+print("Saturation: ", saturation)
+print("Hue: ", hue)
+print("Gain: ", gain)
+print("Exposure: ", exposure)
 
 # allow the camera to warmup, then initialize the average frame, last
 # uploaded timestamp, and frame motion counter
-print "[INFO] warming up..."
+print "Lancement de la caméra ..."
 avg = None
-motionCounter = 0
+motionCounter = False
 
 # capture frames from the camera
 for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -41,8 +71,8 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 
 	# resize the frame, convert it to grayscale, and blur it
 	frame = imutils.resize(frame, width=500)
-	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	frame = cv2.GaussianBlur(frame, (21, 21), 0)
+	frame2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	frame2 = cv2.GaussianBlur(frame2, (21, 21), 0)
 
 	# check to see if the frames should be displayed to screen
 	if conf["show_video"]:
