530aad12183b2c0ce64fe37f3af31a0e62528c33
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2017-01-21 22:01:16 +0100

    test#4

--- pi-home-surveillance/pi_surveillance.py
+++ pi-home-surveillance/pi_surveillance.py
@@ -32,7 +32,7 @@ rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
 # uploaded timestamp, and frame motion counter
 print "Lancement de la camera ..."
 camera.start_preview()
-camera.capture(avg)
+avg = camera.capture()
 motionCounter = False
 
 # capture frames from the camera
@@ -48,7 +48,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 	# check to see if the frames should be displayed to screen
 	if conf["show_video"]:
 		# display the security feed
-		cv2.imshow("Security Feed", frame)
+		cv2.imshow("Security Feed", avg)
 		key = cv2.waitKey(1) & 0xFF
 
 		# if the `q` key is pressed, break from the lop
