5a8219768040b2e8eb466d60c3c774e301892d91
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2017-01-22 10:47:28 +0100

    add: background substraction

--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
+from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
 
@@ -6,20 +7,22 @@ camera = PiCamera()
 rawCapture = PiRGBArray(camera)
 presence = True
 
+camera.capture('background.jpg')
+background = cv2.imread('background.jpg')
+diff = None
 # capture frames from the camera
 for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image and initialize
 	# the timestamp and occupied/unoccupied text
 	frame = f.array
-	# check to see if the frames should be displayed to screen
-	if conf["show_video"]:
-		# display the security feed
-		cv2.imshow("Security Feed", frame)
-		key = cv2.waitKey(1) & 0xFF
-
-		# if the `q` key is pressed, break from the lop
-		if key == ord("q"):
-			break
+	
+	diff = cv2.absdiff(frame,background)
+		
+	cv2.imshow("Security Feed", diff)
+	key = cv2.waitKey(1) & 0xFF
+	# if the `q` key is pressed, break from the lop
+	if key == ord("q"):
+		break
 
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
