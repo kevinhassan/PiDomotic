a28cff3637bfafd35ce41fc3b8a3797818800f1e
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2017-01-22 02:45:59 +0100

    fix: presence detection work

--- pi-home-surveillance/pi_surveillance.py
+++ pi-home-surveillance/pi_surveillance.py
@@ -32,6 +32,7 @@ def detectPresence():
 	# allow the camera to warmup, then initialize the average frame, last
 	# uploaded timestamp, and frame motion counter
 	print "Lancement de la camera ..."
+	time.sleep(conf["camera_warmup_time"])
 	camera.start_preview()
 	avg = None
 
@@ -83,13 +84,16 @@ def detectPresence():
 		# check to see if the frames should be displayed to screen
 		if conf["show_video"]:
 			# display the security feed
-			cv2.imshow("Security Feed", avg)
+			cv2.imshow("Security Feed", frame)
 			key = cv2.waitKey(1) & 0xFF
 
 			# if the `q` key is pressed, break from the lop
 			if key == ord("q"):
 				break
 		# clear the stream in preparation for the next frame
+		if presence == True:
+			break
 		rawCapture.truncate(0)
-	print(presence)
 	return presence
+
+detectPresence()
